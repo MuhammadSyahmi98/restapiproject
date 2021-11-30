541dc8be6d68d1027dcf4dbea79050fafc20e0f2
Muhammad Syahmi <syahmijalil12@gmail.com>
2021-11-30 11:04:20 +0800

    Initial structure
    - Create api route in api.php under route folder
    - Create models and controllers

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Buyer\BuyerController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,10 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::resource('buyers', 'App\Http\Controllers\Buyer\BuyerController', ['only' => ['show', 'index']]);
+Route::resource('sellers', 'App\Http\Controllers\Seller\SellerController', ['only' => ['show', 'index']]);
+Route::resource('products', 'App\Http\Controllers\Product\ProductController', ['only' => ['show', 'index']]);
+Route::resource('transactions', 'App\Http\Controllers\Transaction\TransactionController', ['only' => ['show', 'index']]);
+Route::resource('categories', 'App\Http\Controllers\Category\CategoryController', ['except' => ['create', 'edit']]);
+Route::resource('users', 'App\Http\Controllers\User\UserController', ['except' => ['create', 'edit']]);
